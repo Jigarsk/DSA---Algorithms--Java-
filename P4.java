3c985d683c1ff308e6c40eb9e236aa8e5063d93c
Jigarsk <jigarsk30@gmail.com>
2023-07-16 12:24:19 +0530

    P4 - Find Interest

--- P4.java
+++ P4.java
@@ -3,16 +3,19 @@ import java.util.Scanner;
 public class P4 {
 
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.println("Please enter Principle Amount");
-        int P = input.nextInt();
+        int P = sc.nextInt();
         System.out.println("Please enter Time of Interest in Years");
-        int T = input.nextInt();
+        int T = sc.nextInt();
         System.out.println("Please enter Time of Rate of Interest in %");
-        int R = input.nextInt();
+        int R = sc.nextInt();
         int sum = P * T * R;
         int Interest = sum / 100;
         System.out.println("Interest is " + Interest);
+sc.close();    
     }
+
+    
 }
 
