48af72a042cd1bc2cd5cd13fff29072be11141a3
Jigarsk <jigarsk30@gmail.com>
2023-07-16 12:28:07 +0530

    P7 - Sum of Numbers till User exits by Clicking 0

--- P7.java
+++ P7.java
@@ -2,17 +2,19 @@
 import java.util.Scanner;
 public class P7 {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         int Number,total=0;
         do {
             System.out.println("Please enter a Number");
-            Number = input.nextInt();
+            Number = sc.nextInt();
             total = total + Number;
 
         }while (Number!=0);
         System.out.println("Sum Of Number Is "+total);
 
+        sc.close();
 
 
     }
+    
 }
