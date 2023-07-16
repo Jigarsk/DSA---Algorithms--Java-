26ee4c070d676fdd14d919e64c932e215f880702
Jigarsk <jigarsk30@gmail.com>
2023-07-16 15:46:58 +0530

    P9 - Given Number is Armstrong or Not (153 = (1^3) + (5^3) + (3^3))

---
+++ P9.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class P9 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter a Number");
+        int Num = sc.nextInt();
+        int Armstrong = 0;
+        int original = Num;
+        while(Num>0){
+
+            int temp = Num % 10;
+            temp = (int) Math.pow(temp,3);
+            Armstrong = temp + Armstrong;
+            Num/=10;
+        }
+        if(Armstrong != original){System.out.println("Given Number is Not Armstrong");System.exit(1);}
+        System.out.println("Given Number is Armstrong");
+    }
+}
