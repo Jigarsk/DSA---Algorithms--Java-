931c7319bb5a308f29666df754c44babdba84c7a
Jigarsk <jigarsk30@gmail.com>
2023-07-16 16:43:41 +0530

    P10 - Subtract the Product and Sum of Digits of an Integer

--- P10.java
+++ P10.java
@@ -3,14 +3,14 @@ import java.util.Scanner;
 public class P10 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Please enter a Number ");
-        int Num = sc.nextInt();
+        System.out.println("Please enter a nber ");
+        int n = sc.nextInt();
         int sum = 0, prod = 1;
-        while (Num != 0) {
-            int rem = Num % 10;
+        while (n != 0) {
+            int rem = n % 10;
             sum = sum + rem;
             prod = prod * rem;
-            Num = Num / 10;
+            n = n / 10;
 
         }
         int Result = prod - sum;
