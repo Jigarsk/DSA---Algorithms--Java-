78bc8b94c54fbe1c5e7739742cf1bf9856fb5da8
Jigarsk <jigarsk30@gmail.com>
2023-07-16 12:23:29 +0530

    P3 - Greet Message

--- P3.java
+++ P3.java
@@ -4,8 +4,9 @@ public class P3 {
 
     public static void main(String[] args) {
         System.out.println("Please Enter Your Name");
-        Scanner input = new Scanner(System.in);
-        String name = input.nextLine();
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
         System.out.println("Hello ! Nice To Have You " + name);
+        sc.close();
     }
 }
