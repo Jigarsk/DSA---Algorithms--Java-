bccaef73b72b78374b6fccdf19d796b226c849bc
Jigarsk <jigarsk30@gmail.com>
2023-07-26 10:45:44 +0530

    P7 - Sum Of numbers entered by user and exit when user clicks 0

--- Simple Programs - Conditional Loops/P7.java
+++ Simple Programs - Conditional Loops/P7.java
@@ -1,20 +1,20 @@
-
+//Sum Of numbers entered by user and exit when user clicks 0 
 import java.util.Scanner;
+
 public class P7 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int Number,total=0;
+        int Number, total = 0;
         do {
             System.out.println("Please enter a Number");
             Number = sc.nextInt();
             total = total + Number;
 
-        }while (Number!=0);
-        System.out.println("Sum Of Number Is "+total);
+        } while (Number != 0);
+        System.out.println("Sum Of Number Is " + total);
 
         sc.close();
 
-
     }
-    
+
 }
