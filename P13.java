6649d374b3030ab65df80fb12ff9176d890f6951
Jigarsk <jigarsk30@gmail.com>
2023-07-26 09:22:18 +0530

    P13 - Print the Reverse Number of Given Number

---
+++ P13.java
@@ -0,0 +1,19 @@
+//Print the Reverse Number of Given Number
+
+import java.util.Scanner;
+
+public class P13 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number");
+        int n = sc.nextInt();
+        int ans = 0;
+        while (n > 0) {
+            int rem = n % 10;
+            n /= 10;
+            ans = ans * 10 + rem;
+
+        }
+        System.out.println(ans);
+    }
+}
